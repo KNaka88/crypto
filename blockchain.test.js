7c97d914537cb0423f35c14415889120304f8bb5
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-03-01 18:55:19 -0800

    Added log check for more scenarios

--- blockchain.test.js
+++ blockchain.test.js
@@ -100,21 +100,32 @@ describe('Blockchain', () => {
             });
 
             describe('and the chain is invalid', () => {
-                it('does not replace the chain', () => {
+                beforeEach(() => {
                     newChain.chain[2].hash = 'some-fake-hash';
-
-                    blockchain.replaceChain(newChain.chain);
-
+                    blockchain.replaceChain(newChain.chain);                
+                })
+    
+                it('does not replace the chain', () => {
                     expect(blockchain.chain).toEqual(originalChain);                    
                 });
+
+                it('logs an error', () => {
+                    expect(errorMock).toHaveBeenCalled();
+                });    
             });
 
             describe('and the chain is valid', () => {
-                it('replaces the chain', () => {
+                beforeEach(() => {
                     blockchain.replaceChain(newChain.chain);
+                });
 
+                it('replaces the chain', () => {
                     expect(blockchain.chain).toEqual(newChain.chain);                    
                 });
+
+                it('logs about the chain replacement', () => {
+                    expect(logMock).toHaveBeenCalled();
+                });    
             });
 
         });
