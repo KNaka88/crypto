4bb253e946b02f4b95e692861c6f789e8a3f323f
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-03-16 23:12:11 -0700

    Added POST api/transact  and added additional logic for transactionPool

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 
 const DEFAULT_PORT = 3000;
 let PEER_PORT;
@@ -28,6 +32,28 @@ app.post('/api/mine', (req, res) => {
     pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+    
+    let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey });;
+
+    try {
+        if (transaction) {
+            transaction.update({ senderWallet: wallet, recipient, amount });
+        } else {
+            transaction = wallet.createTransaction({ recipient, amount });
+        }
+    } catch (error) {
+        return res.status(400).json( { type: 'error', message: error.message });
+    }
+
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool', transactionPool);
+
+    res.json({ type: 'success', transaction });
 });
 
 const syncChains = () => {
