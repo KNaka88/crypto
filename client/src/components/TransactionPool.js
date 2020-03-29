e7f14ac66c293df038fa79d46550394a5d3d215b
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-03-29 15:54:38 -0700

    Added mine transaction button

--- client/src/components/TransactionPool.js
+++ client/src/components/TransactionPool.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Transaction from './Transaction';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import history from '../history';
 
 const POOL_INERVAL_MS = 10000;
 
@@ -11,6 +13,16 @@ class TransactionPool extends Component {
         const response = await fetch(`${document.location.origin}/api/transaction-pool-map`);
         this.setState({ transactionPoolMap: await response.json() })    
     };
+
+    fetchMineTransactions = async () => {
+        const response = await fetch(`${document.location.origin}/api/mine-transaction`);
+        if (response.status === 200) {
+            alert('success');
+            history.push('/blocks');
+        } else {
+            alert('The mine-transaction block request did not complete');
+        }
+    }
 
     componentDidMount() {
         this.fetchTransactionPoolMap();
@@ -41,6 +53,13 @@ class TransactionPool extends Component {
                 <div><Link to='/'>Home</Link></div>
                 <h3>Transaction Pool</h3>
                 {this.renderTransactionPool()}
+                <hr/>
+                <Button
+                    bsStyle="danger"
+                    onClick={this.fetchMineTransactions}
+                >
+                    Mine the Transactions
+                </Button>
             </div>
         );
     }
